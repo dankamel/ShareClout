8809c82f6d92b65bba4e318ff6786e9bfd93b0d7
drdan <danielsquashed@aol.com>
2021-07-22 17:29:22 +0200

    Initial Commit

---
+++ ShareClout/ShareCloutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareCloutApp.swift
+//  ShareClout
+//
+//  Created by Daniel Kamel on 22/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ShareCloutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
